cff9ed2d358cf8dd591a5b757a1b352670e1a4bf
kayhide <kayhide@gmail.com>
2017-10-05 20:31:02 +0900

    update home view.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @users = User.without_hidden.order(id: :asc)
-    @records = Record.recent.order(started_at: :desc).group_by(&:user_id)
+    @records = Record.recent.order(started_at: :asc).group_by(&:user_id)
   end
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -6,14 +6,26 @@ RSpec.describe HomeController, type: :controller do
       Timecop.return
     end
 
-    it "returns http success" do
+    it "assigns users" do
+      Timecop.freeze Time.zone.local(2017, 9, 25)
+      users = FactoryGirl.create_list(:user, 2)
+      get :index
+      expect(assigns(:users)).to eq users
+    end
+
+    it "assigns records grouped by user_id" do
       Timecop.freeze Time.zone.local(2017, 9, 25)
       users = FactoryGirl.create_list(:user, 2)
       records = users.map do |user|
-        FactoryGirl.create(:record, user: user, started_at: Time.zone.local(2017, 9, 25, 7, 57, 28))
+        [
+          FactoryGirl.create(:record, user: user, started_at: Time.zone.local(2017, 9, 25, 7, 57, 28)),
+          FactoryGirl.create(:record, user: user, started_at: Time.zone.local(2017, 9, 25, 8, 57, 28))
+        ]
       end
       get :index
       expect(assigns(:users)).to eq users
+      expect(assigns(:records)[users[0].id]).to eq records[0]
+      expect(assigns(:records)[users[1].id]).to eq records[1]
     end
   end
 end
