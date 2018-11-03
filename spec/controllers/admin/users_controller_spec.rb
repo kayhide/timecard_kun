23b08242ba4639a902fdaffb60f13de535d3d199
kayhide <kayhide@gmail.com>
2018-11-03 16:06:45 +0900

    update deprecating specs.

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe Admin::UsersController, type: :controller do
     it "returns a success response" do
       user = FactoryBot.create(:user)
       get :index, params: {}, session: valid_session
-      expect(response).to be_success
+      expect(response).to be_successful
     end
   end
 
@@ -22,14 +22,14 @@ RSpec.describe Admin::UsersController, type: :controller do
     it "returns a success response" do
       user = FactoryBot.create(:user)
       get :show, params: {id: user.to_param}, session: valid_session
-      expect(response).to be_success
+      expect(response).to be_successful
     end
   end
 
   describe "GET #new" do
     it "returns a success response" do
       get :new, params: {}, session: valid_session
-      expect(response).to be_success
+      expect(response).to be_successful
     end
   end
 
@@ -37,7 +37,7 @@ RSpec.describe Admin::UsersController, type: :controller do
     it "returns a success response" do
       user = FactoryBot.create(:user)
       get :edit, params: {id: user.to_param}, session: valid_session
-      expect(response).to be_success
+      expect(response).to be_successful
     end
   end
 
@@ -64,7 +64,7 @@ RSpec.describe Admin::UsersController, type: :controller do
     context "with invalid params" do
       it "returns a success response (i.e. to display the 'new' template)" do
         post :create, params: {user: invalid_attributes}, session: valid_session
-        expect(response).to be_success
+        expect(response).to be_successful
       end
     end
   end
@@ -95,7 +95,7 @@ RSpec.describe Admin::UsersController, type: :controller do
       it "returns a success response (i.e. to display the 'edit' template)" do
         user = FactoryBot.create(:user)
         put :update, params: {id: user.to_param, user: invalid_attributes}, session: valid_session
-        expect(response).to be_success
+        expect(response).to be_successful
       end
     end
   end
